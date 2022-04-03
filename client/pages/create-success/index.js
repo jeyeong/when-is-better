cc0f9edf1372415c28007bf2bf52b2f666a74ed0
Don <don.assamongkol1@gmail.com>
2022-04-03 13:48:05 -0500

    finish success page

--- client/pages/create-success/index.js
+++ client/pages/create-success/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 
 import styles from '../../styles/CreateSuccess.module.css';
 import { useRouter } from 'next/router';
+import { Button } from '@mui/material';
 
 const DOMAIN_NAME = 'https://when-is-better.vercel.app/';
 
@@ -25,24 +26,33 @@ const CreateSuccess = () => {
         />
       </Head>
       <div className={styles.createsuccess}>
-        <h1>Success</h1>
+        <h1 className={styles.header_font}>Success</h1>
         <span>Forward this link to your event attendees!</span>
-        <div
-          className={styles.createsuccess__linkbox}
-        >{`${DOMAIN_NAME}form?event_id=${event_id}`}</div>
+        <div className={styles.margin_top}>
+          <Button
+            variant="contained"
+            style={{
+              borderRadius: '20px',
+              padding: '0.5rem 0.9rem',
+              fontSize: '16px',
+              width: '80vw',
+              textTransform: 'lowercase',
+              backgroundColor: '#fff',
+              border: '3px solid #087f5b',
+              color: '#000',
+            }}
+            onClick={() =>
+              navigator.clipboard.writeText(
+                `${DOMAIN_NAME}form?event_id=${event_id}`
+              )
+            }
+          >
+            {`${DOMAIN_NAME}form?event_id=${event_id}`}
+          </Button>
+        </div>
       </div>
     </>
   );
 };
 
 export default CreateSuccess;
-
-{
-  /* <button
-onClick={() =>
-  navigator.clipboard.writeText(`${DOMAIN_NAME}${event_id}`)
-}
->
-Copy
-</button> */
-}
