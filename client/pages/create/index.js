fb78c16258063103eebb2cdbcaa8948f1bf2490f
Je Yeong - Pycharm <jeyeong@uchicago.edu>
2022-04-03 11:12:34 -0500

    hardcoded timeslots

--- client/pages/create/index.js
+++ client/pages/create/index.js
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import TextField from '@mui/material/TextField'
-import Button from '@mui/material/Button'
-import AdapterLuxon from '@mui/lab/AdapterLuxon'
-import LocalizationProvider from '@mui/lab/LocalizationProvider'
-import DatePicker from '@mui/lab/DatePicker'
-import Hammer from 'react-hammerjs'
+import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import AdapterLuxon from '@mui/lab/AdapterLuxon';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
+import Hammer from 'react-hammerjs';
+import { DateTime, Duration } from 'luxon';
 
-import styles from '../../styles/Create.module.css'
+import { generateTimeSlotArray } from '../../models/timeslots';
 
-import timeslots from '../../models/timeslots'
+import styles from '../../styles/Create.module.css';
 
 const TIMES = [
   '8am',
@@ -24,31 +25,30 @@ const TIMES = [
   '6pm',
   '7pm',
   '8pm',
-]
+];
 
-
-const TimeSelectionStep = ({ selections, setSelections }) => {
+const TimeSelection = ({ timeslots, setTimeslots }) => {
   const [editLock, setEditLock] = useState(
     new Array(4).fill(0).map((day) => Array(12).fill(false))
-  )
+  );
   const [firstAction, setFirstAction] = useState({
     fixed: false,
     isSelection: false,
-  })
+  });
 
   const onPaint = (e, dayIndex) => {
-    const timeIndex = findTimeIndex(e.center)
+    const timeIndex = findTimeIndex(e.center);
 
     if (!firstAction.fixed) {
       setFirstAction({
         fixed: true,
-        isSelection: !selections[dayIndex][timeIndex],
-      })
+        isSelection: !timeslots[dayIndex][timeIndex],
+      });
     }
 
     if (!editLock[dayIndex][timeIndex]) {
-      setSelections(
-        selections.map((day, i) =>
+      setTimeslots(
+        timeslots.map((day, i) =>
           i === dayIndex
             ? day.map((time, j) =>
                 j === timeIndex && !(firstAction.isSelection && time)
@@ -57,29 +57,36 @@ const TimeSelectionStep = ({ selections, setSelections }) => {
               )
             : day
         )
-      )
+      );
       setEditLock(
         editLock.map((day, i) =>
           i === dayIndex
             ? day.map((time, j) => (j === timeIndex ? true : time))
             : day
         )
-      )
+      );
     }
-  }
+  };
 
   const resetEditLocks = () => {
-    setEditLock(new Array(4).fill(0).map((day) => Array(12).fill(false)))
-    setFirstAction({ fixed: false, isSelection: false })
-  }
+    setEditLock(new Array(4).fill(0).map((day) => Array(12).fill(false)));
+    setFirstAction({ fixed: false, isSelection: false });
+  };
 
   const findTimeIndex = (coords) => {
-    return Math.floor((coords.y - 95) / 40)
-  }
+    return Math.floor((coords.y - 95) / 40);
+  };
 
   const createEvent = () => {
-    console.log('create event')
-  }
+    fetch('https://when-is-better-backend.herokuapp.com/', {
+      method: 'GET',
+      mode: 'cors',
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+      });
+  };
 
   return (
     <div className={styles.timeselection}>
@@ -89,7 +96,7 @@ const TimeSelectionStep = ({ selections, setSelections }) => {
         onTouchEnd={resetEditLocks}
         onMouseUp={resetEditLocks}
       >
-        {selections.map((day, i) => (
+        {timeslots.map((day, i) => (
           <Hammer
             onPan={(e) => onPaint(e, i)}
             onTap={(e) => onPaint(e, i)}
@@ -113,20 +120,30 @@ const TimeSelectionStep = ({ selections, setSelections }) => {
         Create
       </Button>
     </div>
-  )
-}
+  );
+};
 
 const CreateForm = () => {
-  const [step, setStep] = useState(0)
-  const [selections, setSelections] = useState(
-    new Array(4).fill(0).map((day) => Array(12).fill(false))
-  )
-    return (
-      <TimeSelectionStep
-        selections={selections}
-        setSelections={setSelections}
-      />
-    )
-}
+  const start = DateTime.fromObject({
+    year: 2022,
+    month: 4,
+    day: 4,
+    hour: 8,
+  });
+  const end = DateTime.fromObject({
+    year: 2022,
+    month: 4,
+    day: 7,
+    hour: 20,
+  });
+  const delta_duration = Duration.fromObject({ minutes: 60 });
 
-export default CreateForm
+  console.log(generateTimeSlotArray(start, end, delta_duration));
+
+  const [timeslots, setTimeslots] = useState(
+    new Array(4).fill(0).map((day) => Array(12).fill(false))
+  );
+  return <TimeSelection timeslots={timeslots} setTimeslots={setTimeslots} />;
+};
+
+export default CreateForm;
