1a7a45cbe9b77c1fee45e14af84978922c9bfcda
Don <don.assamongkol1@gmail.com>
2022-04-03 11:54:17 -0500

    styled date picker

--- client/components/DatePicker.js
+++ client/components/DatePicker.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import AdapterLuxon from '@mui/lab/AdapterLuxon';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DatePicker from '@mui/lab/DatePicker';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
-export const BasicDatePicker = ({ setDate }) => {
+export const BasicDatePicker = ({ setDate, label }) => {
   const [value, setValue] = useState(null);
 
   const onChange = (newValue) => {
@@ -18,10 +18,21 @@ export const BasicDatePicker = ({ setDate }) => {
   return (
     <LocalizationProvider dateAdapter={AdapterLuxon}>
       <DatePicker
-        label="DatePicker"
+        label={label}
         value={value}
         onChange={onChange}
-        renderInput={(params) => <TextField {...params} />}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            sx={{
+              width: 0.4,
+              mx: 2,
+              color: 'primary.main',
+              // color: '#087f5b',
+              // bgcolor: 'white',
+            }}
+          />
+        )}
       />
     </LocalizationProvider>
   );
---
+++ client/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+import styles from '../styles/Footer.module.css';
+
+export const Footer = () => {
+  return <div className={styles.footer}>&#169; WhenIsBetter 2022</div>;
+};
+
+{
+  /* <div><FaGithub /></div> */
+}
