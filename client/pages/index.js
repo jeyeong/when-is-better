fbae60c34f7527a92bb1264c77a4628d78cd25b3
Don <don.assamongkol1@gmail.com>
2022-04-02 21:22:32 -0500

    work on front end

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,7 +3,7 @@ import { Button } from '@mui/material'
 
 const LandingPage = () => {
   return (
-    <div>
+    <div className={styles.main_container}>
       <h1>Landing Page</h1>
       <Link href="/create">
         <Button variant="contained">Start</Button>
