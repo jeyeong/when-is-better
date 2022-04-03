2dbf16b88504f1d01a14e964c88c91c110a456cf
Je Yeong - Pycharm <jeyeong@uchicago.edu>
2022-04-03 00:12:56 -0500

    don's sick homepage

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,71 @@
 import Link from 'next/link'
 import { Button } from '@mui/material'
+import styles from '../styles/Home.module.css'
+
+import { FaCalendarAlt } from 'react-icons/fa'
+import { IconContext } from 'react-icons'
+import Head from 'next/head'
 
 const LandingPage = () => {
   return (
-    <div>
-      <h1>Landing Page</h1>
-      <Link href="/create" passHref>
-        <Button variant="contained">Start</Button>
-      </Link>
-    </div>
+    <>
+      <Head>
+        {/* for the font */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Open+Sans:ital,wght@0,400;0,800;1,400&family=Raleway+Dots&family=Raleway:wght@100;400;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <div className={styles.image_wrapper}>
+        <img
+          src="icon.svg"
+          alt="Clock Icon Image"
+          className={styles.main_image}
+        />
+      </div>
+
+      <div className={styles.wrapper}>
+        <div className={styles.diagonal_box_top_left}>
+          <div className={styles.undo_diagonal_top_left}>
+            <div className={styles.margin_x}>
+              <h1 className={styles.title}>
+                When Is <span className={styles.accented}>Better</span>
+              </h1>
+            </div>
+
+            <div className={styles.diagonal_box_top_right}>
+              <div className={styles.undo_diagonal_top_right}>
+                <div className={styles.margin_x}>
+                  <p className={styles.description}>
+                    When Is Better is your hub for scheduling your hangouts
+                    easily, eliminating the hassle of back-and-forth messages so
+                    you can get back to what matters most.
+                  </p>
+                  <div className={styles.center_button}>
+                    <Link href="/create">
+                      <Button
+                        variant="contained"
+                        style={{
+                          backgroundColor: '#087f5b',
+                          'border-radius': '50px',
+                          padding: '1rem 2rem',
+                          fontSize: '1rem',
+                        }}
+                      >
+                        Get Started
+                      </Button>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
