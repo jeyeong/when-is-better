5378f096a05df65141da1195c5bc998fbab7a936
Don Assamongkol <donassamongkol@Dons-MacBook-Air.local>
2022-05-04 13:55:45 -0500

    added TZ frontend

--- client/components/create-page/OptionsMenu.js
+++ client/components/create-page/OptionsMenu.js
@@ -1,35 +1,59 @@
 import React from 'react';
 import styles from '../../styles/OptionsMenu.module.css';
-
 import Box from '@mui/material/Box';
-import Slider from '@mui/material/Slider';
 import { useState } from 'react';
 
+import TimezoneSelect from 'react-timezone-select';
+
+import { styled } from '@mui/material/styles';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import MuiToggleButton from '@mui/material/ToggleButton';
+
+/* Define our own styling
+https://stackoverflow.com/questions/69707814/set-selected-background-color-of-mui-togglebutton
+*/
+const ToggleButton = styled(MuiToggleButton)({
+  '&.Mui-selected, &.Mui-selected:hover': {
+    color: 'white',
+    backgroundColor: '#087f5b',
+  },
+});
+
 export const OptionsMenu = () => {
-  const marks = [
-    {
-      value: 15,
-      label: '15',
-      scaledValue: 1,
-    },
-    {
-      value: 30,
-      label: '30',
-      scaledValue: 2,
-    },
-    {
-      value: 60,
-      label: '60',
-      scaledValue: 3,
-    },
+  /* Code for TimeZone */
+  const [selectedTimezone, setSelectedTimezone] = useState(
+    Intl.DateTimeFormat().resolvedOptions().timeZone
+  ); // use default timezone of user
+  const currTZ = JSON.stringify(selectedTimezone, null, 2);
+  console.log('currTZ: ', currTZ);
+
+  /* Code for Time Increment */
+  const [timeInterval, setTimeInterval] = useState('15');
+  const handleChange = (event, newTimeInterval) => {
+    if (newTimeInterval == null) {
+      return; /* Prevent users hitting the same button twice --> no null state */
+    }
+    setTimeInterval(newTimeInterval);
+  };
+
+  const children = [
+    <ToggleButton value="15" key="15" disableRipple={true}>
+      15
+    </ToggleButton>,
+    <ToggleButton value="30" key="30" disableRipple={true}>
+      30
+    </ToggleButton>,
+    <ToggleButton value="60" key="60" disableRipple={true}>
+      60
+    </ToggleButton>,
   ];
 
-  function valuetext(value) {
-    return `${value}°C`;
-  }
+  const control = {
+    value: timeInterval,
+    onChange: handleChange,
+    exclusive: true,
+  };
 
-  const DEFAULT_TIME_INTERVAL = 15;
-  const [timeInterval, setTimeInterval] = useState(DEFAULT_TIME_INTERVAL);
   return (
     <div className="container-padding-sm">
       <div className={`${styles.options_container}`}>
@@ -39,25 +63,28 @@ export const OptionsMenu = () => {
             <li>end Date</li>
             <li>Hour Start</li>
             <li>Hour End</li>
-            <li>Timezone: react-timezone-select</li>
+            <li>
+              Timezone
+              <div className="select-wrapper">
+                <TimezoneSelect
+                  value={selectedTimezone}
+                  onChange={setSelectedTimezone}
+                />
+              </div>
+            </li>
             <li>
               Increment
-              {/* <Box>
-                <Slider
-                  aria-label="Custom marks"
-                  defaultValue={60}
-                  getAriaValueText={valuetext}
-                  step={null}
-                  marks={marks}
-                  min={15}
-                  max={60}
-                  valueLabelDisplay="auto"
-                  sx={{ width: 200, color: 'green' }}
-                  onChange={(e, val) => setTimeInterval(val)}
-                  value={timeInterval}
-                />
-              </Box> */}
-              {/* for even spacing: https://stackoverflow.com/questions/61792449/material-ui-slider-changing-values-using-scale */}
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}
+              >
+                <ToggleButtonGroup size="small" {...control}>
+                  {children}
+                </ToggleButtonGroup>
+              </Box>
             </li>
           </ul>
         </div>
